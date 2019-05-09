bab2bd374d1b3739a0123368f6f3151966c07eae
qobilidop <qobilidop@gmail.com>
2019-05-09 16:03:53 -0700

    Update gizmo package
    - Rename version
    - Reduce variants
    - Deal with FFTW flags

--- repos/gizmo/packages/gizmo/package.py
+++ repos/gizmo/packages/gizmo/package.py
@@ -18,22 +18,29 @@ class Gizmo(MakefilePackage):
 
     homepage = "http://www.tapir.caltech.edu/~phopkins/Site/GIZMO.html"
 
-    version('public', hg='https://bitbucket.org/phopkins/gizmo-public')
+    version('develop-public', hg='https://bitbucket.org/phopkins/gizmo-public')
 
-    variant('hdf5', default=True)
     variant('grackle', default=False)
 
     depends_on('mpi')
+
+    depends_on('fftw@2:2.2+mpi')
     depends_on('gsl')
-    depends_on('fftw-api@2')
-    depends_on('fftw+mpi')
-    depends_on('hdf5', when='+hdf5')
+    depends_on('hdf5')
     depends_on('grackle', when='+grackle')
 
     def edit(self, spec, prefix):
         env['MPICC'] = spec['mpi'].mpicc
         env['MPICXX'] = spec['mpi'].mpicxx
         env['OPENMP_FLAG'] = self.compiler.openmp_flag
+
+        opt = []
+        if '+double' in spec['fftw']:
+            opt += ['-DDOUBLEPRECISION_FFTW']
+        elif '~float' in spec['fftw']:
+            opt += ['-DNOTYPEPREFIX_FFTW']
+        env['OPT'] = ' '.join(opt)
+
         with open('Makefile.systype', 'w') as f:
             f.write(MAKE_TEXT)
         touch('Config.sh')
